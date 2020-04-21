6fa6bafd37454811706d318fbc479be1f76c78fa
ljhymca <ljhymca@naver.com>
2020-04-22 03:11:32 +0900

    메뉴화면 예제

---
+++ app/src/main/java/org/techtown/push/sampleintent/MainActivity.java
@@ -0,0 +1,33 @@
+package org.techtown.push.sampleintent;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    public static final int REQUEST_CODE_MENU = 101;
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.menuButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MenuActivity.class);
+                startActivityForResult(intent, REQUEST_CODE_MENU);
+            }
+        });
+    }
+}
